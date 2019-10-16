e36333cf9fe314fb2b7644b5c720dea21ac80e3f
Clarazhangbw <55799109+Clarazhangbw@users.noreply.github.com>
2019-10-16 23:57:56 +0800

    Create TreeNode.java

---
+++ TreeNode.java
@@ -0,0 +1,16 @@
+package ruangong2;
+
+import java.awt.List;
+
+public class TreeNode  { //定义树结点的结构
+	
+		Object val;
+		TreeNode left;
+		TreeNode right;
+		
+		public TreeNode(Object val)
+		{
+			this.val = val;
+		}
+		
+}
